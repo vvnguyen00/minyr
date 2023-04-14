bae2c6ae0f3d6a9849b8ca7ddde649a6ef43169a
vvnguyen00 <112411896+vvnguyen00@users.noreply.github.com>
2023-04-14 13:24:24 +0200

    Update yr.go

---
+++ yr/yr.go
@@ -0,0 +1,42 @@
+package yr
+
+import (
+	"fmt"
+	"strconv"
+	//"strings"
+	//"errors"
+	"github.com/vvnguyen00/misc/conv"
+)
+
+func CelsiusToFahrenheitString(celsius string) (string, error) {
+	var fahrFloat float64
+	var err error
+	if celsiusFloat, err := strconv.ParseFloat(celsius, 64); err == nil {
+		fahrFloat = conv.CelsiusToFahrenheit(celsiusFloat)
+	}
+	fahrString := fmt.Sprintf("%.1f", fahrFloat)
+	return fahrString, err
+}
+
+// Forutsetter at vi kjenner strukturen i filen og denne implementasjon 
+// er kun for filer som inneholder linjer hvor det fjerde element
+// på linjen er verdien for temperaturaaling i grader celsius
+func CelsiusToFahrenheitLine(line string) (string, error) {
+
+        dividedString := strings.Split(line, ";")
+	var err error
+	
+	if (len(dividedString) == 4) {
+		dividedString[3], err = CelsiusToFahrenheitString(dividedString[3])
+		if err != nil {
+			return "", err
+		}
+	} else {
+		return "", errors.New("linje har ikke forventet format")
+	}
+	return strings.Join(dividedString, ";"), nil
+	
+	/*	
+	return "Kjevik;SN39040;18.03.2022 01:50;42.8", err
+        */
+}
